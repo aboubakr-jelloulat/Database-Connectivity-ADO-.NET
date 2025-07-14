061009de2150e34e94b82357e71143e8e55785f8
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-07-14 12:03:28 +0100

    Parameterized Query With Like

--- Connect To Sql Server/Program.cs
+++ Connect To Sql Server/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// ***********  special thanks to Programming Advices   ***************
+
+using System;
 using System.Linq;
 using System.Net;
 using System.Data;
@@ -152,6 +154,194 @@ public class Program
 
     }
 
+
+    /*
+     * 
+     *  'abc%'	Starts with "abc"
+        '%abc'	Ends with "abc"
+        '%abc%'	Contains "abc" anywhere
+     * 
+     */
+
+    static void SearchContactsStartsWith(string StartsWith)
+    {
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName LIKE '' + @StartsWith +'%'";
+
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@StartsWith", StartsWith);
+
+        /*
+         * 
+         * you can use this :
+         * 
+               string query = "SELECT * FROM Contacts WHERE FirstName LIKE @pattern";
+                command.Parameters.AddWithValue("@pattern", StartsWith + "%");
+         *
+         */
+
+        try
+        {
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+
+        }
+
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+
+    }
+
+
+    static void SearchContactsEndsWith(string EndsWith)
+    {
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName LIKE '%' + @EndsWith + ''";
+
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@EndsWith", EndsWith);
+
+        /*
+         * you can use this :
+         * 
+                string query = "SELECT * FROM Contacts WHERE FirstName LIKE @pattern";
+                command.Parameters.AddWithValue("@pattern", "%" + EndsWith);
+         * 
+         */
+
+        try
+        {
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+
+        }
+
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+
+    }
+
+
+    static void SearchContactsContains(string Contains)
+    {
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName LIKE '%' + @Contains + '%'";
+
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Contains", Contains);
+
+        /*
+         * you can use this :
+         * 
+                string query = "SELECT * FROM Contacts WHERE FirstName LIKE @pattern";
+                command.Parameters.AddWithValue("@pattern", "%" + Contains + "%");
+
+         * 
+         */
+
+        try
+        {
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+
+        }
+
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+
+    }
+
+
     public static void Main(string[] args)
     {
         //  ************   bascis link   Print all contacts   ***********
@@ -161,8 +351,27 @@ public class Program
 
         // ************   Parametrized query   ***********
 
-        // PrintAllContactsWithFirstName("jane");
-        //PrintAllContactsWithFirstNameAndCountry("jane", 1);
+
+        //  PrintAllContactsWithFirstName("jane");
+        //  PrintAllContactsWithFirstNameAndCountry("jane", 1);
+
+
+        //  ************** Parameterized Query With "Like"     **********
+
+
+        //Console.WriteLine("--------Contacts starts with 'j'");
+
+        //SearchContactsStartsWith("j");
+
+        //Console.WriteLine("-------Contacts Ends with 'ne'");
+        //SearchContactsEndsWith("ne");
+
+        //Console.WriteLine("-------Contacts Contains with 'ae'");
+        //SearchContactsContains("ae");
+
+
+       
+
 
 
 
