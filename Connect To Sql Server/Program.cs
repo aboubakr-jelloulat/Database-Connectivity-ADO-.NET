ff30e4aad390552d0e6b54412537cfb4c86b3b6f
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-07-14 10:58:58 +0100

    Remove .vs and ignore unnecessary files

---
+++ Connect To Sql Server/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Data;
+using System.Data.SqlClient;
+
+public class Program
+{
+    static string connectionString = "Server=.; Database=ContactsDB; User Id = sa; Password=sa123456";
+
+    static void PrintAllContacts()
+    {
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts";
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        try
+        {
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader[0];
+                string firstName = (string)reader[1];
+                string lastName = (string)reader["LastName"];
+                string email = reader["Email"].ToString();
+                string phone = reader["Phone"].ToString();
+                string address = reader["Address"].ToString();
+                int    countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID : {contactID}");
+                Console.WriteLine($"First Name : {firstName}");
+                Console.WriteLine($"Last Name  : {lastName}");
+                Console.WriteLine($"Email      : {email}");
+                Console.WriteLine($"Phone      : {phone}");
+                Console.WriteLine($"Address    : {address}");
+                Console.WriteLine($"Country ID : {countryID}");
+
+                Console.WriteLine();
+
+            }
+
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error : " + ex.Message);
+        }
+    }
+
+
+    public static void Main(string[] args)
+    {
+        PrintAllContacts();
+
+       Console.ReadKey();
+    }
+}
