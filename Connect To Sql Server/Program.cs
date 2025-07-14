a20afe05866842fef38d64aeb9894358ec16121e
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-07-14 11:38:41 +0100

    Parametrized query

--- Connect To Sql Server/Program.cs
+++ Connect To Sql Server/Program.cs
@@ -52,12 +52,119 @@ public class Program
         }
     }
 
+    static void PrintAllContactsWithFirstName(string UserName)
+    {
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName = @UserName";
+
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@UserName", UserName);
+
+        try
+        {
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+
+    static void PrintAllContactsWithFirstNameAndCountry(string FirstName, int CountryID)
+    {
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = "SELECT * FROM Contacts WHERE FirstName = @FirstName and CountryID=@CountryID";
+
+
+        SqlCommand command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@FirstName", FirstName);
+        command.Parameters.AddWithValue("@CountryID", CountryID);
+
+        try
+        {
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int contactID = (int)reader["ContactID"];
+                string firstName = (string)reader["FirstName"];
+                string lastName = (string)reader["LastName"];
+                string email = (string)reader["Email"];
+                string phone = (string)reader["Phone"];
+                string address = (string)reader["Address"];
+                int countryID = (int)reader["CountryID"];
+
+                Console.WriteLine($"Contact ID: {contactID}");
+                Console.WriteLine($"Name: {firstName} {lastName}");
+                Console.WriteLine($"Email: {email}");
+                Console.WriteLine($"Phone: {phone}");
+                Console.WriteLine($"Address: {address}");
+                Console.WriteLine($"Country ID: {countryID}");
+                Console.WriteLine();
+            }
+
+            reader.Close();
+            connection.Close();
+
+        }
+
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+
+    }
 
     public static void Main(string[] args)
     {
-        // 1 -  bascis link   Print all contacts  
+        //  ************   bascis link   Print all contacts   ***********
 
-        //PrintAllContacts();
+        //  PrintAllContacts();
+
+
+        // ************   Parametrized query   ***********
+
+        // PrintAllContactsWithFirstName("jane");
+        //PrintAllContactsWithFirstNameAndCountry("jane", 1);
+
+
 
         Console.ReadKey();
     }
