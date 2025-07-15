4318152bb46ee1d28a81d999f05b86a3c744ee7a
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-07-15 11:58:26 +0100

    update data

--- 2 insertAddUpdateDelete/Program.cs
+++ 2 insertAddUpdateDelete/Program.cs
@@ -108,6 +108,55 @@ public class Program
 
     }
 
+
+    static void UpdateContact(int ContactID, stContact ContactInfo)
+    {
+
+
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        string query = @"Update  Contacts  
+                            set FirstName = @FirstName, 
+                                LastName = @LastName, 
+                                Email = @Email, 
+                                Phone = @Phone, 
+                                Address = @Address, 
+                                CountryID = @CountryID
+                                where ContactID = @ContactID";
+
+        SqlCommand command = new SqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@ContactID", ContactID);
+        command.Parameters.AddWithValue("@FirstName", ContactInfo.FirstName);
+        command.Parameters.AddWithValue("@LastName", ContactInfo.LastName);
+        command.Parameters.AddWithValue("@Email", ContactInfo.Email);
+        command.Parameters.AddWithValue("@Phone", ContactInfo.Phone);
+        command.Parameters.AddWithValue("@Address", ContactInfo.Address);
+        command.Parameters.AddWithValue("@CountryID", ContactInfo.CountryID);
+
+        try
+        {
+            connection.Open();
+
+            int rowsAffected = command.ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+            {
+                Console.WriteLine("Record Updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Record Upadate failed.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+
+    }
+
     public static void Main(string[] args)
     {
         //  *********** insert / Add   Data *************
@@ -139,6 +188,7 @@ public class Program
 
 
 
+        /*
         stContact Contact = new stContact
         {
             FirstName = "Sander",
@@ -150,6 +200,31 @@ public class Program
         };
 
         AddNewContactAndGetID(Contact);
+
+        */
+
+
+
+
+        //  ***********  Update data  *************
+
+
+
+        /*
+        stContact ContactInfo = new stContact
+        {
+            FirstName = "Jone",
+            LastName = "Matioe",
+            Email = "jane@gmail.com",
+            Phone = "000000000",
+            Address = "foot Street, usa",
+            CountryID = 1
+        };
+
+        UpdateContact(1, ContactInfo);
+
+
+        */
 
 
         Console.ReadKey();
